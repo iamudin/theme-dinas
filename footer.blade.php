2b56210913cddef33ece342aae67935be1c04f62
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-10-03 08:28:13 +0700

    push

---
+++ footer.blade.php
@@ -0,0 +1,198 @@
+<!-- Footer -->
+<footer class="site-footer">
+    <!-- newsletter part -->
+    <div class="bg-primary dez-newsletter">
+        <div class="container equal-wraper">
+            <form class="dzSubscribe" action="script/mailchamp.php" method="post">
+                <div class="row">
+                    <div class="col-md-4 col-lg-4 position-relative">
+                        <div class="icon-bx-wraper equal-col p-t30 p-b20 left">
+                            <div class="icon-lg text-primary radius"> <a href="javascript:void(0);" class="icon-cell"><i
+                                        class="far fa-envelope"></i></a> </div>
+                            <div class="icon-content"> <strong
+                                    class="text-black text-uppercase font-18">Subscribe</strong>
+                                <h2 class="dez-tilte text-uppercase">Our Newsletter</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4 position-relative">
+                        <div class="dzSubscribeMsg"></div>
+                        <div class="input-group equal-col p-t40 p-b20">
+                            <input name="dzEmail" placeholder="Email address" required="required" class="form-control"
+                                type="email">
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-lg-3 position-relative offset-lg-1 offset-md-1">
+                        <div class="equal-col p-t40 p-b20 skew-subscribe">
+                            <button name="submit" value="Submit" type="submit"
+                                class="site-button-secondry button-skew z-index1">
+                                <span>Subscribe</span><i class="fa fa-angle-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- footer top part -->
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_about">
+                        <div class="logo-footer">
+                            <a href="index.html" class="logo-light"><img src="images/logo-white.png" alt=""></a>
+                        </div>
+                        <p><strong>Our mission </strong>is to provide the best value and service to our clients. Here we
+                            are always ready to help you. We are devoted to the task to construct your dream to fit all
+                            your needs and preference. We realize that our success starts and ends with our employees so
+                            we try to provide excellent work to our clients with a level of expertise.</p>
+                        <ul class="dez-social-icon dez-border">
+                            <li><a target="_blank" href="https://www.facebook.com/dexignzone/"><i
+                                        class="fab fa-facebook-f"></i></a></li>
+                            <li><a target="_blank" href="https://twitter.com/dexignzones"><i
+                                        class="fab fa-twitter"></i></a></li>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/dexignzone"><i
+                                        class="fab fa-linkedin-in"></i></a></li>
+                            <li><a target="_blank" href="https://www.instagram.com/dexignzone/"><i
+                                        class="fab fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget recent-posts-entry">
+                        <h4 class="m-b15 text-uppercase">Recent Post</h4>
+                        <div class="dez-separator-outer m-b10">
+                            <div class="dez-separator bg-white style-skew"></div>
+                        </div>
+                        <div class="widget-post-bx">
+                            <div class="widget-post clearfix">
+                                <div class="dez-post-media"> <img src="images/blog/recent-blog/pic1.jpg" alt=""
+                                        width="200" height="143"> </div>
+                                <div class="dez-post-info">
+                                    <div class="dez-post-header">
+                                        <h6 class="post-title text-uppercase"><a href="blog-single.html">Creative
+                                                Construct</a></h6>
+                                    </div>
+                                    <div class="dez-post-meta">
+                                        <ul>
+                                            <li class="post-author">By <a href="javascript:void(0);">Jemmy</a></li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 28</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget-post clearfix">
+                                <div class="dez-post-media"> <img src="images/blog/recent-blog/pic2.jpg" alt=""
+                                        width="200" height="160"> </div>
+                                <div class="dez-post-info">
+                                    <div class="dez-post-header">
+                                        <h6 class="post-title text-uppercase"><a href="blog-single.html">Professional
+                                                Services</a></h6>
+                                    </div>
+                                    <div class="dez-post-meta">
+                                        <ul>
+                                            <li class="post-author">By <a href="javascript:void(0);">Jone Doe</a></li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 28</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="widget-post clearfix">
+                                <div class="dez-post-media"> <img src="images/blog/recent-blog/pic3.jpg" alt=""
+                                        width="200" height="160"> </div>
+                                <div class="dez-post-info">
+                                    <div class="dez-post-header">
+                                        <h6 class="post-title  text-uppercase"><a href="blog-single.html">Construction
+                                                Planning</a></h6>
+                                    </div>
+                                    <div class="dez-post-meta">
+                                        <ul>
+                                            <li class="post-author">By <a href="javascript:void(0);">Frenchi</a></li>
+                                            <li class="post-comment"><i class="fa fa-comments"></i> 28</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_services">
+                        <h4 class="m-b15 text-uppercase">Our services</h4>
+                        <div class="dez-separator-outer m-b10">
+                            <div class="dez-separator bg-white style-skew"></div>
+                        </div>
+                        <ul>
+                            <li><a href="services-2.html">Residential Construction</a></li>
+                            <li><a href="services-2.html">Office Construction</a></li>
+                            <li><a href="services-2.html">Wall Painting</a></li>
+                            <li><a href="services-2.html">Window Construction</a></li>
+                            <li><a href="services-2.html">Commercial Construction</a></li>
+                            <li><a href="services-2.html">Office Construction</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6 footer-col-4">
+                    <div class="widget widget_getintuch">
+                        <h4 class="m-b15 text-uppercase">Contact us</h4>
+                        <div class="dez-separator-outer m-b10">
+                            <div class="dez-separator bg-white style-skew"></div>
+                        </div>
+                        <ul>
+                            <li><i class="fas fa-map-marker-alt"></i><strong>address</strong> demo address #8901 Marmora
+                                Road Chi Minh City, Vietnam </li>
+                            <li><i class="fa fa-phone"></i><strong>phone</strong>0800-123456 (24/7 Support Line)</li>
+                            <li><i class="fa fa-fax"></i><strong>FAX</strong>(123) 123-4567</li>
+                            <li><i class="fa fa-envelope"></i><strong>email</strong>info@demo.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- footer bottom part -->
+    <div class="footer-bottom footer-line">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 text-left">
+                    <span>© Copyright <span class="current-year">2024</span></span>
+                </div>
+                <div class="col-lg-4 col-md-4 text-center">
+                    <span> Design With <i class="fa fa-heart text-primary heart"></i> By DexignZone </span>
+                </div>
+                <div class="col-lg-4 col-md-4 text-right">
+                    <a href="about-1.html"> About Us</a>
+                    <a href="faq-1.html"> FAQs</a>
+                    <a href="contact.html"> Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer END-->
+<!-- scroll top button -->
+<button class="scroltop fa fa-arrow-up"></button>
+</div>
+<!-- JavaScript  files ========================================= -->
+<script src="/template/dinas/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="/template/dinas/plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="/template/dinas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="/template/dinas/plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+<script src="/template/dinas/plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+<script src="/template/dinas/plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+<script src="/template/dinas/plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="/template/dinas/plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="/template/dinas/plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="/template/dinas/plugins/perfect-scrollbar/js/perfect-scrollbar.min.js"></script><!-- Perfect Scrollbar -->
+<script src="/template/dinas/plugins/masonry/masonry-4.2.2.js"></script><!-- MASONRY -->
+<script src="/template/dinas/plugins/masonry/isotope.pkgd.min.js"></script><!-- MASONRY -->
+<script src="/template/dinas/plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+<script src="/template/dinas/js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+<script src="/template/dinas/js/dz.carousel.js"></script><!-- SORTCODE FUCTIONS  -->
+<script src="/template/dinas/js/dz.ajax.js"></script><!-- CONTACT JS  -->
+
+
+</body>
+
+</html>
---
+++ home.blade.php
@@ -0,0 +1,40 @@
+<!-- Content -->
+
+<div class="page-content">
+    <!-- inner page banner -->
+    <div class="dez-bnr-inr overlay-black-middle" style="background-image:url(images/background/bg4.jpg);">
+        <div class="container">
+            <div c ass="dez-bnr-inr-entry">
+                <h1 class="text-white">Header Style 6</h1>
+            </div>
+        </div>
+
+    </div>
+    <!-- inner page banner E
+        D -->
+    <!-- Breadcrumb row -->
+    <div class="breadcrumb-row">
+        <div class="container">
+
+            <li><a href="javascript:void(0);">Home</a></li>
+            <li>Header Style 6</li>
+            </ul>
+
+        </div>
+        <!-- Breadcrumb row END -->
+
+        <div class="clearfix">
+            <!-- About Company -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <img src="images/header/header-style-6.jpg" alt="">
+
+                    </div>
+                </div>
+            </div>
+            <!-- About Company END -->
+        </div>
+
+        <!-- Content END-->
